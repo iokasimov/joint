52cf551bcd80aefd92fad0f35814d7a9f9b235d0
Murat Kasimov <iokasimov.m@gmail.com>
2019-08-25 11:50:03 +0300

    Define Transformer typeclass

---
+++ Control/Joint/Transformer.hs
@@ -0,0 +1,13 @@
+module Control.Joint.Transformer (Transformer (..)) where
+
+import Control.Joint.Core (type (~>))
+import Control.Joint.Composition (Composition)
+
+class Composition t => Transformer t where
+	{-# MINIMAL lay, wrap #-}
+	type Schema (t :: * -> *) (u :: * -> *) = (r :: * -> *) | r -> t u
+	lay :: Functor u => u ~> Schema t u
+	wrap :: Applicative u => t ~> Schema t u
+
+infixr 1 :>
+type (:>) t u a = Transformer t => Schema t u a
