b7fc861b131f5954357f677d9f9b402ef5f6f238
Murat Kasimov <iokasimov.m@gmail.com>
2019-08-25 11:42:52 +0300

    Rename > to :=, remove reverse functors composition

--- Control/Joint/Core.hs
+++ Control/Joint/Core.hs
@@ -1,16 +1,14 @@
 module Control.Joint.Core where
 
 infixr 1 :.
-infixr 1 .:
+infixr 0 :=
+infixr 0 ~>
 
 -- | Functor composition
 type (:.) t u a = t (u a)
--- | Reversed functor composition
-type (.:) t u a = u (t a)
 
--- | Functor object
-infixr 0 >
-type (>) t a = t a
+-- | Functor's object
+type (:=) t a = t a
 
 -- | Natural transformation
-type (~>) t u = forall a . (Functor t, Functor u) => t a -> u a
+type (~>) t u = forall a . t a -> u a
--- Control/Joint/Schemes/TU.hs
+++ Control/Joint/Schemes/TU.hs
@@ -1,10 +1,10 @@
 module Control.Joint.Schemes.TU (TU (..)) where
 
-import Control.Joint.Core (type (:.), type (>))
+import Control.Joint.Core (type (:.), type (:=))
 import Control.Joint.Composition (Composition (Primary, unwrap))
 
-newtype TU t u a = TU (t :. u > a)
+newtype TU t u a = TU (t :. u := a)
 
 instance Composition (TU t u) where
-	type Primary (TU t u) a = t :. u > a
+	type Primary (TU t u) a = t :. u := a
 	unwrap (TU x) = x
