6eae1c8cc2878e93bfcf42654892b905fb8095ef
Murat Kasimov <iokasimov.m@gmail.com>
2019-12-31 19:57:42 +0300

    Rename Reader to Configured

--- Control/Joint/Base/Configured.hs
+++ Control/Joint/Base/Configured.hs
@@ -1,11 +1,11 @@
-module Control.Joint.Base.Reader where
+module Control.Joint.Base.Configured where
 
 import Control.Joint.Composition (Composition (Primary, run))
 import Control.Joint.Transformer (Transformer (Schema, embed, build, unite))
 import Control.Joint.Modulator (Modulator ((-<$>-)))
 import Control.Joint.Schemes.TU (TU (TU))
 
-newtype Reader e a = Reader (e -> a)
+newtype Configured e a = Configured (e -> a)
 
 instance Functor u => Functor (TU ((->) e) u) where
 	fmap f (TU x) = TU $ \r -> f <$> x r
@@ -17,18 +17,18 @@ instance Applicative u => Applicative (TU ((->) e) u) where
 instance (Applicative u, Monad u) => Monad (TU ((->) e) u) where
 	TU x >>= f = TU $ \e -> x e >>= ($ e) . run . f
 
-instance Composition (Reader e) where
-	type Primary (Reader e) a = (->) e a
-	run (Reader x) = x
+instance Composition (Configured e) where
+	type Primary (Configured e) a = (->) e a
+	run (Configured x) = x
 
-instance Transformer (Reader e) where
-	type Schema (Reader e) u = TU ((->) e) u
+instance Transformer (Configured e) where
+	type Schema (Configured e) u = TU ((->) e) u
 	embed x = TU . const $ x
 	build x = TU $ pure <$> run x
 	unite = TU
 
-instance Modulator (Reader e) where
+instance Modulator (Configured e) where
 	f -<$>- (TU x) = TU $ f <$> x
 
-ask :: Reader e e
-ask = Reader $ \e -> e
+ask :: Configured e e
+ask = Configured $ \e -> e
