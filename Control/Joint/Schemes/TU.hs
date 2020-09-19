a95af212e1090077f7a871f5362bda21366d441f
Murat Kasimov <iokasimov.m@gmail.com>
2020-09-19 18:09:42 +0300

    Define MonadTrans instances for TU and UT schemes

--- Control/Joint/Schemes/TU.hs
+++ Control/Joint/Schemes/TU.hs
@@ -1,5 +1,7 @@
 module Control.Joint.Schemes.TU (TU (..)) where
 
+import "transformers" Control.Monad.Trans.Class (MonadTrans (lift))
+
 import Control.Joint.Core (type (:.), type (:=))
 import Control.Joint.Abilities (Interpreted (Primary, run))
 
@@ -8,3 +10,6 @@ newtype TU t u a = TU (t :. u := a)
 instance Interpreted (TU t u) where
 	type Primary (TU t u) a = t :. u := a
 	run (TU x) = x
+
+instance Monad t => MonadTrans (TU t) where
+	lift = TU . return
--- Control/Joint/Schemes/UT.hs
+++ Control/Joint/Schemes/UT.hs
@@ -1,5 +1,7 @@
 module Control.Joint.Schemes.UT (UT (..)) where
 
+import "transformers" Control.Monad.Trans.Class (MonadTrans (lift))
+
 import Control.Joint.Core (type (:.), type (:=))
 import Control.Joint.Abilities.Interpreted (Interpreted (Primary, run))
 
@@ -8,3 +10,6 @@ newtype UT t u a = UT (u :. t := a)
 instance Interpreted (UT t u) where
 	type Primary (UT t u) a = u :. t := a
 	run (UT x) = x
+
+instance (forall u . Functor u, Monad t) => MonadTrans (UT t) where
+	lift x = UT $ return <$> x
