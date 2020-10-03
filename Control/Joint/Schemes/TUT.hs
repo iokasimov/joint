870b4a089f131d92ccb255b8cc983ecf5a47b062
Murat Kasimov <iokasimov.m@gmail.com>
2020-10-03 08:27:09 +0300

    Try to replace Traversable with Distributive in TUT

--- Control/Joint/Schemes/TUT.hs
+++ Control/Joint/Schemes/TUT.hs
@@ -1,5 +1,7 @@
 module Control.Joint.Schemes.TUT (TUT (..)) where
 
+import "adjunctions" Data.Functor.Adjunction (Adjunction (leftAdjunct, rightAdjunct))
+import "distributive" Data.Distributive (Distributive (distribute))
 import "transformers" Control.Monad.Trans.Class (MonadTrans (lift))
 
 import Control.Joint.Core (type (:.), type (:=))
@@ -11,5 +13,6 @@ instance Interpreted (TUT t t' u) where
 	type Primary (TUT t t' u) a = t :. u :. t' := a
 	run (TUT x) = x
 
-instance (Applicative t, Applicative t') => MonadTrans (TUT t t') where
-	lift x = TUT . pure $ pure <$> x
+-- TODO: try to replace Traversable on Monad here
+instance (Adjunction t' t, Distributive t') => MonadTrans (TUT t t') where
+	lift x = TUT $ distribute $ (leftAdjunct id <$> x)
